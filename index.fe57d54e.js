6c0910ab0f70d501721ac1eb43a9351eb7822710
Kirill Gasan <dp160190gkj@gmail.com>
2024-08-05 11:01:32 +0300

    make build

---
+++ index.fe57d54e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("ul").addEventListener("click",function(t){if("IMG"===t.target.tagName||"A"===t.target.tagName){t.preventDefault();var r=t.target.closest("a").getAttribute("href");e.setAttribute("src",r)}});
+//# sourceMappingURL=index.fe57d54e.js.map
